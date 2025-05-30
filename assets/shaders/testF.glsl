eabd7d4113e2c9c9889596d3c2ef700f926fa43d
AlfredoSoto1 <108200951+AlfredoSoto1@users.noreply.github.com>
2025-05-30 17:15:40 -0400

    OpenGL and Vulkan Enabled and working

---
+++ assets/shaders/testF.glsl
@@ -0,0 +1,9 @@
+//#shader fragment
+#version 460 core
+
+layout(location = 0) out vec4 color;
+
+void main(void) {
+	
+	color = vec4(1.0);
+}
---
+++ assets/shaders/testV.glsl
@@ -0,0 +1,9 @@
+//#shader vertex
+#version 460 core
+
+layout(location = 0) in vec2 position;
+
+void main(void) {
+
+	gl_Position = vec4(position, 1.0, 1.0);
+}
